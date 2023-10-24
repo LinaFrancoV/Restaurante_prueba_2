b4109a771f0b31363a202c8c5e2a10446fe82ae9
LinaFrancoV <lpfrancov@gmail.com>
2023-10-24 09:20:31 -0500

    Modificación de nombres de variables, creación de clase, sus variables e inicializadores

--- Juego_danger_words/danger_words_v14.6.swift
+++ Juego_danger_words/danger_words_v14.6.swift
@@ -1,66 +1,76 @@
 import Foundation
 
-let wordsForTheGame = ["algorithm", "language", "compiler", "variable", "function", "loop", "class", "object", "inheritance", "polymorphism", "encapsulation", "modularity", "debugging", "integration", "development", "frontend", "backend", "database", "API", "framework", "library", "programmer", "syntax", "compilation", "interpretation", "link", "optimization", "repository", "constant"]
-
-func generateFromRandomWord(_ wordsForTheGame: [String]) -> String {
-    let randomWord = wordsForTheGame.randomElement() ?? ""
-    return randomWord
-}
-
-func letterIdentifierL(_ word: String, _ characterArray: [Character]) {
-    for l in word {
-        if characterArray.contains(l) {
-            print(l, terminator: " ")
-        } else {
-            print("_", terminator: " ")
-        }
-    }
-    print()
-}
-
-func take(_ gl: [Character]) -> Character {
-    print("Enter a letter: ", terminator: "")
-    var l = Character((readLine() ?? "").lowercased())
-
-    while l.isLetter == false || String(l).count != 1 {
-        print("Please enter only one letter: ", terminator: "")
-        l = Character((readLine() ?? "").lowercased())
-    }
-
-    while gl.contains(l) {
-        print("You already guessed that letter, try another one: ", terminator: "")
-        l = Character((readLine() ?? "").lowercased())
-    }
-    return l
-}
-
-func dangerWords() {
-    print("Welcome to the Danger Words game!")
-    let secret = cw(w)
+class PlayDangerWords {
+ 
+    let wordsForTheGame: [String] = ["algorithm", "language", "compiler", "variable", "function", "loop", "class", "object", "inheritance", "polymorphism", "encapsulation", "modularity", "debugging", "integration", "development", "frontend", "backend", "database", "API", "framework", "library", "programmer", "syntax", "compilation", "interpretation", "link", "optimization", "repository", "constant"]
+    let secret: "String"
     var guessed: [Character] = []
-    var trys = 8
-
-    while true {
-        sw(secret, guessed)
-        let L = take(guessed)
-
-        if secret.contains(L) {
-            guessed.append(L)
-            print("Well done! You guessed a letter.")
-        } else {
-            trys -= 1
-            print("Sorry, that letter is not in the word.")
-            print("You have \(trys) lives remaining.")
-
-            if trys == 0 {
-                print("Game Over! The word was: \(secret)")
-                break
-            } else if Set(secret).isSubset(of: Set(guessed)) {
-                print("Congratulations! You guessed the word: \(secret)")
-                break
+    var trys: Int
+    
+    init(guessed: [Character], trys: Int, secret: "String", word: String, characterArray: [Character]) {
+        self.guessed = guessed
+        self.trys = trys
+        self.secret = secret
+        self.word = word
+        self.characterArray = characterArray
+    }
+    
+    //Generador de una palabra aleatoria dentro del array 
+    func generateFromRandomWord( wordsForTheGame: [String]) -> String {
+        let randomWord = wordsForTheGame.randomElement() ?? ""
+        return randomWord
+    }
+    func letterIdentifierL( word: String, characterArray: [Character]) {
+        for l in word {
+            if characterArray.contains(l) {
+                print(l, terminator: " ")
+            } else {
+                print("_", terminator: " ")
+            }
+        }
+        print()
+    }
+    func guessTheLetter( characterArray: [Character]) -> Character {
+        print("Enter a letter: ", terminator: "")
+        var letterEnteredByUser = Character((readLine() ?? "").lowercased())
+        
+        while letterEnteredByUser.isLetter == false || String(letterEnteredByUser ).count != 1 {
+            print("Please enter only one letter: ", terminator: "")
+            letterEnteredByUser = Character((readLine() ?? "").lowercased())
+        }
+        while characterArray.contains(l) {
+            print("You already guessed that letter, try another one: ", terminator: "")
+            letterEnteredByUser  = Character((readLine() ?? "").lowercased())
+        }
+        return letterEnteredByUser
+    }
+    func dangerWords(secret: String, guessed: [Character] = [], trys = Int) {
+        print("Welcome to the Danger Words game!")
+        
+        while true {
+            sw(secret, guessed)
+            let L = take(guessed)
+            
+            if secret.contains(L) {
+                guessed.append(L)
+                print("Well done! You guessed a letter.")
+            } else {
+                trys -= 1
+                print("Sorry, that letter is not in the word.")
+                print("You have \(trys) lives remaining.")
+                
+                if trys == 0 {
+                    print("Game Over! The word was: \(secret)")
+                    break
+                } else if Set(secret).isSubset(of: Set(guessed)) {
+                    print("Congratulations! You guessed the word: \(secret)")
+                    break
+                }
             }
         }
     }
+    
 }
-
-dangerWords()
+var trys = 8
+let secret = cw(w)
+PlayDangerWords()
